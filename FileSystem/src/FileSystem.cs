8e602e8271cea6eeaae146540f9a89eb15bf6d47
Mauro van der Gun <mauro@vdgun.net>
2020-02-26 23:53:05 -0400

    add documentation

--- FileSystem/src/FileSystem.cs
+++ FileSystem/src/FileSystem.cs
@@ -11,17 +11,30 @@ namespace SharpGrip.FileSystem
 {
     public class FileSystem : IFileSystem, IDisposable
     {
+        /// <summary>
+        /// The adapters.
+        /// </summary>
         public IList<IAdapter> Adapters { get; set; } = new List<IAdapter>();
 
+        /// <summary>
+        /// FileSystem constructor.
+        /// </summary>
         public FileSystem()
         {
         }
 
+        /// <summary>
+        /// FileSystem constructor.
+        /// </summary>
+        /// <param name="adapters">The adapters.</param>
         public FileSystem(IList<IAdapter> adapters)
         {
             Adapters = adapters;
         }
 
+        /// <summary>
+        /// Disposes the attached adapters.
+        /// </summary>
         public void Dispose()
         {
             foreach (var adapter in Adapters)
@@ -30,6 +43,12 @@ namespace SharpGrip.FileSystem
             }
         }
 
+        /// <summary>
+        /// Return a file.
+        /// </summary>
+        /// <param name="path">The path (including prefix) to the file.</param>
+        /// <returns>The file.</returns>
+        /// <exception cref="FileNotFoundException">Thrown if the file does not exists at the given path.</exception>
         public IFile GetFile(string path)
         {
             var prefix = GetPrefix(path);
@@ -40,6 +59,12 @@ namespace SharpGrip.FileSystem
             return adapter.GetFile(path);
         }
 
+        /// <summary>
+        /// Returns a directory.
+        /// </summary>
+        /// <param name="path">The path (including prefix) to the directory.</param>
+        /// <returns>The directory.</returns>
+        /// <exception cref="DirectoryNotFoundException">Thrown if the file does not exists at the given path.</exception>
         public IDirectory GetDirectory(string path)
         {
             var prefix = GetPrefix(path);
@@ -50,6 +75,11 @@ namespace SharpGrip.FileSystem
             return adapter.GetDirectory(path);
         }
 
+        /// <summary>
+        /// Returns files present at the provided path.
+        /// </summary>
+        /// <param name="path">The path (including prefix) to list the files at.</param>
+        /// <returns>The files present at the provided path.</returns>
         public IEnumerable<IFile> GetFiles(string path = "")
         {
             var prefix = GetPrefix(path);
@@ -60,6 +90,11 @@ namespace SharpGrip.FileSystem
             return adapter.GetFiles(path);
         }
 
+        /// <summary>
+        /// Returns directories present at the provided path.
+        /// </summary>
+        /// <param name="path">The path (including prefix) to list the directories at.</param>
+        /// <returns>The directories present at the provided path.</returns>
         public IEnumerable<IDirectory> GetDirectories(string path = "")
         {
             var prefix = GetPrefix(path);
@@ -70,6 +105,11 @@ namespace SharpGrip.FileSystem
             return adapter.GetDirectories(path);
         }
 
+        /// <summary>
+        /// Checks if a file exists at the provided path.
+        /// </summary>
+        /// <param name="path">The path (including prefix) to check.</param>
+        /// <returns>True if the file exists at the provided path, False otherwise.</returns>
         public bool FileExists(string path)
         {
             var prefix = GetPrefix(path);
@@ -80,6 +120,11 @@ namespace SharpGrip.FileSystem
             return adapter.FileExists(path);
         }
 
+        /// <summary>
+        /// Checks if a directory exists at the provided path.
+        /// </summary>
+        /// <param name="path">The path (including prefix) to check.</param>
+        /// <returns>True if the directory exists at the provided path, False otherwise.</returns>
         public bool DirectoryExists(string path)
         {
             var prefix = GetPrefix(path);
@@ -90,6 +135,11 @@ namespace SharpGrip.FileSystem
             return adapter.DirectoryExists(path);
         }
 
+        /// <summary>
+        /// Creates a file at the provided path.
+        /// </summary>
+        /// <param name="path">The path (including prefix) where to create the file at.</param>
+        /// <returns>The created file.</returns>
         public Stream CreateFile(string path)
         {
             var prefix = GetPrefix(path);
@@ -100,142 +150,242 @@ namespace SharpGrip.FileSystem
             return adapter.CreateFile(path);
         }
 
-        public DirectoryInfo CreateDirectory(string path)
+        /// <summary>
+        /// Creates a directory at the provided path.
+        /// </summary>
+        /// <param name="path">The path (including prefix) where to create the directory at.</param>
+        public void CreateDirectory(string path)
         {
             var prefix = GetPrefix(path);
             path = GetPath(path);
+            var adapter = GetAdapter(prefix);
+            adapter.Connect();
 
-            return GetAdapter(prefix).CreateDirectory(path);
+            adapter.CreateDirectory(path);
         }
 
+        /// <summary>
+        /// Deletes a file at the provided path.
+        /// </summary>
+        /// <param name="path">The path (including prefix) where to delete the file at.</param>
         public void DeleteFile(string path)
         {
             var prefix = GetPrefix(path);
             path = GetPath(path);
+            var adapter = GetAdapter(prefix);
+            adapter.Connect();
 
-            GetAdapter(prefix).DeleteFile(path);
+            adapter.DeleteFile(path);
         }
 
-        public void DeleteDirectory(string path, bool recursive = false)
+        /// <summary>
+        /// Deletes a directory at the provided path.
+        /// </summary>
+        /// <param name="path">The path (including prefix) where to delete the directory at.</param>
+        public void DeleteDirectory(string path)
         {
             var prefix = GetPrefix(path);
             path = GetPath(path);
+            var adapter = GetAdapter(prefix);
+            adapter.Connect();
 
-            GetAdapter(prefix).DeleteDirectory(path, recursive);
+            adapter.DeleteDirectory(path);
         }
 
+        /// <summary>
+        /// Reads a file at the provided path.
+        /// </summary>
+        /// <param name="path">The path (including prefix) where to read the file at.</param>
+        /// <returns>The file contents.</returns>
         public async Task<byte[]> ReadFile(string path)
         {
             var prefix = GetPrefix(path);
             path = GetPath(path);
+            var adapter = GetAdapter(prefix);
+            adapter.Connect();
 
-            return await GetAdapter(prefix).ReadFile(path);
+            return await adapter.ReadFile(path);
         }
 
+        /// <summary>
+        /// Reads a text file at the provided path.
+        /// </summary>
+        /// <param name="path">The path (including prefix) where to read the file at.</param>
+        /// <returns>The file contents.</returns>
         public async Task<string> ReadTextFile(string path)
         {
             var prefix = GetPrefix(path);
             path = GetPath(path);
+            var adapter = GetAdapter(prefix);
+            adapter.Connect();
 
-            return await GetAdapter(prefix).ReadTextFile(path);
+            return await adapter.ReadTextFile(path);
         }
 
+        /// <summary>
+        /// Copies a file from a source path to a destination path.
+        /// </summary>
+        /// <param name="sourcePath">The path (including prefix) where to copy the file from.</param>
+        /// <param name="destinationPath">The path (including prefix) where to copy the file to.</param>
+        /// <param name="overwrite">If a file at the destination path exists overwrite it.</param>
+        /// <exception cref="FileExistsException">Thrown if the file exists at the given path and parameter "overwrite" = false.</exception>
         public async Task CopyFile(string sourcePath, string destinationPath, bool overwrite = false)
         {
             var sourcePrefix = GetPrefix(sourcePath);
             sourcePath = GetPath(sourcePath);
             var sourceAdapter = GetAdapter(sourcePrefix);
+            sourceAdapter.Connect();
 
             var destinationPrefix = GetPrefix(destinationPath);
             destinationPath = GetPath(destinationPath);
             var destinationAdapter = GetAdapter(destinationPrefix);
+            destinationAdapter.Connect();
 
             await destinationAdapter.WriteFile(destinationPath, await sourceAdapter.ReadFile(sourcePath), overwrite);
         }
 
+        /// <summary>
+        /// Moves a file from a source path to a destination path.
+        /// </summary>
+        /// <param name="sourcePath">The path (including prefix) where to move the file from.</param>
+        /// <param name="destinationPath">The path (including prefix) where to move the file to.</param>
+        /// <param name="overwrite">If a file at the destination path exists overwrite it.</param>
+        /// <exception cref="FileExistsException">Thrown if the file exists at the given path and parameter "overwrite" = false.</exception>
         public async Task MoveFile(string sourcePath, string destinationPath, bool overwrite = false)
         {
             var sourcePrefix = GetPrefix(sourcePath);
             sourcePath = GetPath(sourcePath);
             var sourceAdapter = GetAdapter(sourcePrefix);
+            sourceAdapter.Connect();
 
             var destinationPrefix = GetPrefix(destinationPath);
             destinationPath = GetPath(destinationPath);
             var destinationAdapter = GetAdapter(destinationPrefix);
+            destinationAdapter.Connect();
 
             await destinationAdapter.WriteFile(destinationPath, await sourceAdapter.ReadFile(sourcePath), overwrite);
-            await sourceAdapter.DeleteFile(sourcePath);
+            sourceAdapter.DeleteFile(sourcePath);
         }
 
-        public void WriteFile(string path, byte[] contents, bool overwrite = false)
+        /// <summary>
+        /// Writes byte array contents to a file at the provided path.
+        /// </summary>
+        /// <param name="path">The path (including prefix) where to write the byte array contents to.</param>
+        /// <param name="contents">The file byte array contents.</param>
+        /// <param name="overwrite">If a file at the destination path exists overwrite it.</param>
+        /// <exception cref="FileExistsException">Thrown if the file exists at the given path and parameter "overwrite" = false.</exception>
+        public async Task WriteFile(string path, byte[] contents, bool overwrite = false)
         {
             var prefix = GetPrefix(path);
             path = GetPath(path);
+            var adapter = GetAdapter(prefix);
+            adapter.Connect();
 
-            GetAdapter(prefix).WriteFile(path, contents, overwrite);
+            await adapter.WriteFile(path, contents, overwrite);
         }
 
-        public void WriteFile(string path, string contents, bool overwrite = false)
+        /// <summary>
+        /// Writes string contents to a file at the provided path.
+        /// </summary>
+        /// <param name="path">The path (including prefix) where to write the string contents to.</param>
+        /// <param name="contents">The file string contents.</param>
+        /// <param name="overwrite">If a file at the destination path exists overwrite it.</param>
+        /// <exception cref="FileExistsException">Thrown if the file exists at the given path and parameter "overwrite" = false.</exception>
+        public async Task WriteFile(string path, string contents, bool overwrite = false)
         {
             var prefix = GetPrefix(path);
             path = GetPath(path);
+            var adapter = GetAdapter(prefix);
+            adapter.Connect();
 
-            GetAdapter(prefix).WriteFile(path, contents, overwrite);
+            await adapter.WriteFile(path, contents, overwrite);
         }
 
-        public void AppendFile(string path, byte[] contents)
+        /// <summary>
+        /// Writes byte array contents to a file at the provided path.
+        /// </summary>
+        /// <param name="path">The path (including prefix) where to write the byte array contents to.</param>
+        /// <param name="contents">The file byte array contents.</param>
+        public async Task AppendFile(string path, byte[] contents)
         {
             var prefix = GetPrefix(path);
             path = GetPath(path);
+            var adapter = GetAdapter(prefix);
+            adapter.Connect();
 
-            GetAdapter(prefix).AppendFile(path, contents);
+            await adapter.AppendFile(path, contents);
         }
 
-        public void AppendFile(string path, string contents)
+        /// <summary>
+        /// Writes string contents to a file at the provided path.
+        /// </summary>
+        /// <param name="path">The path (including prefix) where to write the string contents to.</param>
+        /// <param name="contents">The file string contents.</param>
+        public async Task AppendFile(string path, string contents)
         {
             var prefix = GetPrefix(path);
             path = GetPath(path);
+            var adapter = GetAdapter(prefix);
+            adapter.Connect();
 
-            GetAdapter(prefix).AppendFile(path, contents);
+            await adapter.AppendFile(path, contents);
         }
 
+        /// <summary>
+        /// Returns an adapter by prefix.
+        /// </summary>
+        /// <param name="prefix">The adapter's prefix.</param>
+        /// <returns>The adapter.</returns>
+        /// <exception cref="NoAdaptersRegisteredException">Thrown when no adapters are registered with the file system.</exception>
+        /// <exception cref="AdapterNotFoundException">Thrown when an adapter could not be found via the provided prefix.</exception>
         public IAdapter GetAdapter(string prefix)
         {
-            if (Adapters == null)
+            if (Adapters == null || Adapters.Count == 0)
             {
-                throw new AdaptersNullException("No adapters registered with the file system.");
-            }
-
-            if (Adapters.Count == 0)
-            {
-                throw new AdaptersEmptyException("The provided list of adapters cannot be empty.");
+                throw new NoAdaptersRegisteredException();
             }
 
             if (Adapters.All(adapter => adapter.Prefix != prefix))
             {
                 var adapters = string.Join(", ", Adapters.Select(adapter => adapter.Prefix).ToArray());
 
-                throw new AdapterNotFoundException($"No adapter found with prefix '{prefix}'. Registered adapters are: {adapters}.");
+                throw new AdapterNotFoundException(prefix, adapters);
             }
 
             return Adapters.First(adapter => adapter.Prefix == prefix);
         }
 
+        /// <summary>
+        /// Returns the prefix from a prefixed path.
+        /// </summary>
+        /// <param name="path">The prefixed path.</param>
+        /// <returns>The prefix.</returns>
         private static string GetPrefix(string path)
         {
             return ResolvePrefixAndPath(path)[0];
         }
 
+        /// <summary>
+        /// Returns the path from a prefixed path.
+        /// </summary>
+        /// <param name="path">The prefixed path.</param>
+        /// <returns>The path.</returns>
         private static string GetPath(string path)
         {
             return ResolvePrefixAndPath(path)[1];
         }
 
+        /// <summary>
+        /// Resolves the prefix and path from a prefixed path.
+        /// </summary>
+        /// <param name="path">The prefixed path.</param>
+        /// <returns>The prefix and path.</returns>
+        /// <exception cref="PrefixNotFoundInPathException">Thrown when a prefix in the provided path could not be found.</exception>
         private static string[] ResolvePrefixAndPath(string path)
         {
             if (!path.Contains("://"))
             {
-                throw new PrefixNotFoundException($"No prefix found in path '{path}'.");
+                throw new PrefixNotFoundInPathException(path);
             }
 
             return path.Split(new[] {"://"}, StringSplitOptions.None);
--- FileSystem/src/IFileSystem.cs
+++ FileSystem/src/IFileSystem.cs
@@ -9,43 +9,24 @@ namespace SharpGrip.FileSystem
     public interface IFileSystem
     {
         public IList<IAdapter> Adapters { get; set; }
-
         public IAdapter GetAdapter(string prefix);
-
         public IFile GetFile(string path);
-
         public IDirectory GetDirectory(string path);
-
         public IEnumerable<IFile> GetFiles(string path = "");
-
         public IEnumerable<IDirectory> GetDirectories(string path = "");
-
         public bool FileExists(string path);
-
         public bool DirectoryExists(string path);
-
         public Stream CreateFile(string path);
-
-        public DirectoryInfo CreateDirectory(string path);
-
+        public void CreateDirectory(string path);
         public void DeleteFile(string path);
-
-        public void DeleteDirectory(string path, bool recursive = false);
-
+        public void DeleteDirectory(string path);
         public Task<byte[]> ReadFile(string path);
-
         public Task<string> ReadTextFile(string path);
-
         public Task CopyFile(string sourcePath, string destinationPath, bool overwrite = false);
-
         public Task MoveFile(string sourcePath, string destinationPath, bool overwrite = false);
-
-        public void WriteFile(string path, byte[] contents, bool overwrite = false);
-
-        public void WriteFile(string path, string contents, bool overwrite = false);
-
-        public void AppendFile(string path, byte[] contents);
-
-        public void AppendFile(string path, string contents);
+        public Task WriteFile(string path, byte[] contents, bool overwrite = false);
+        public Task WriteFile(string path, string contents, bool overwrite = false);
+        public Task AppendFile(string path, byte[] contents);
+        public Task AppendFile(string path, string contents);
     }
 }
